71a5b849a97921c94ac9a25373a15922a51e87ce
John Van Ham <john@vanh.am>
2019-09-20 21:31:36 +0100

    Fix code indentation issues (take 2)

--- ghost-acf-form.php
+++ ghost-acf-form.php
@@ -77,9 +77,9 @@ add_action( 'init', 'GhostACFForm_create_formentry_cpt', 0 );
 //Form entry save
 function GhostACFForm_after_save_post( $post_id ) {
 
-    // Bail early if not a ghost-form-entry post
-    if( get_post_type($post_id) !== 'ghost-form-entry' ) {
-        return $post_id;
+	// Bail early if not a ghost-form-entry post
+	if( get_post_type($post_id) !== 'ghost-form-entry' ) {
+		return $post_id;
 	}
 
 	//We don't need to run this if we're in the admin area
@@ -95,18 +95,18 @@ function GhostACFForm_after_save_post( $post_id ) {
 	$form = acf_get_field_group($ghost_acf_form_key);
 	$ghost_acf_form_name = $form['title'];
 
-    // Get custom fields (field group exists for content_form)
-    $first_name = get_field('first_name', $post_id);
-    $last_name = get_field('last_name', $post_id);
-    $name = $first_name . " " . $last_name;
-    $email = get_field('email', $post_id);
-    $message = get_field('message', $post_id);
+	// Get custom fields (field group exists for content_form)
+	$first_name = get_field('first_name', $post_id);
+	$last_name = get_field('last_name', $post_id);
+	$name = $first_name . " " . $last_name;
+	$email = get_field('email', $post_id);
+	$message = get_field('message', $post_id);
 
-    // Create/update post
-    $data['ID']         = $post_id;
-    $title              = $ghost_acf_form_name . ' | ' . $name . ' - ' . $email;
-    $data['post_title'] = $title;
-    $data['post_name']  = sanitize_title( $title );
+	// Create/update post
+	$data['ID']         = $post_id;
+	$title              = $ghost_acf_form_name . ' | ' . $name . ' - ' . $email;
+	$data['post_title'] = $title;
+	$data['post_name']  = sanitize_title( $title );
 	wp_update_post( $data );
 
 	// Update post with the form ID it was created by
@@ -122,18 +122,18 @@ function GhostACFForm_after_save_post( $post_id ) {
 	}
 
 	//Put together email
-    $headers = 'Content-Type: text/html; charset=UTF-8'. "\r\n";
-    $headers .= 'From: ' . $name . ' <' . $email . '>' . "\r\n";
+	$headers = 'Content-Type: text/html; charset=UTF-8'. "\r\n";
+	$headers .= 'From: ' . $name . ' <' . $email . '>' . "\r\n";
 	$subject = "New submission for form '" . $ghost_acf_form_name . "' from " . $name . " <".$email.">";
 
 	$body = file_get_contents(__DIR__ . '/email-templates/email-header.html');
 
 	$body .= "<h2><strong>" . $ghost_acf_form_name . "</strong> - New submission</h2>\r\n";
 	$body .= "A new submission has been entered for this form.<br /><br />\r\n";
-    $body .= '<strong>From:</strong> ' . $name ."<br />\r\n";
+	$body .= '<strong>From:</strong> ' . $name ."<br />\r\n";
 	$body .= '<strong>Email:</strong> <a href="mailto:'.$email.'">' . $email . "</a><br /><br />\r\n";
 	if(!empty($message)) {
-        $body .= "<strong>Message:</strong><br />\r\n";
+		$body .= "<strong>Message:</strong><br />\r\n";
 		$body .= nl2br(strip_tags(str_replace("<br />","\n", str_replace("<br>", "\n", $message))));
 		$body .= "<br /><br />\n";
 	}
@@ -151,7 +151,7 @@ function GhostACFForm_after_save_post( $post_id ) {
 				</td>
 			</tr>
 		</tbody>
-	</table>';
+	</table>'; //Buttons in emails are awful!
 
 	$body .= file_get_contents(__DIR__ . '/email-templates/email-footer.html');
 
@@ -165,7 +165,7 @@ add_action('acf/save_post', 'GhostACFForm_after_save_post', 20);
 //Add acf_form_head if shortcode detected
 function GhostACFForm_enqueue_acf_form() {
 	if( is_admin() )
-	return;
+		return;
 
 	global $post;
 	if( has_shortcode( $post->post_content, 'ghost_acf_form' ) ){
@@ -178,7 +178,7 @@ add_action( 'wp', 'GhostACFForm_enqueue_acf_form', 15 );
 function GhostACFForm_shortcode( $atts ) {
 
 	if( is_admin() || wp_doing_ajax() )
-	return;
+		return;
 
 	$args = shortcode_atts( array(
 		'form_name' => '',
@@ -225,8 +225,8 @@ function GhostACFForm_render_form($form) {
 			case 'first_name':
 			case 'last_name':
 			case 'email':
-				$checked_fields++;
-				break;
+			$checked_fields++;
+			break;
 		}
 	}
 	if($checked_fields != 3) {
@@ -236,33 +236,33 @@ function GhostACFForm_render_form($form) {
 	$output = "";
 	$output .= '<div id="ghost-acf-form">';
 
-    $sent = !empty($_GET['entry']) && $_GET['entry'] == "confirmed";
-    if(!$sent) {
+	$sent = !empty($_GET['entry']) && $_GET['entry'] == "confirmed";
+	if(!$sent) {
 		ob_start();
 		acf_form(array(
 			'field_groups' => $field_groups,
-            'post_id' => 'new_post',
-            'post_title' => false,
-            'new_post' => array(
-                'post_type' => 'ghost-form-entry',
-                'post_status'	=> 'publish',
-            ),
-            'submit_value' => __("Submit", 'ghost-acf-form'),
-            'return' => get_permalink() . '?entry=confirmed#ghost-acf-form',
-            'honeypot' => true,
-            'html_submit_button'	=> '<input type="submit" class="btn btn-primary" value="%s" />',
-            'recaptcha' => true,
+			'post_id' => 'new_post',
+			'post_title' => false,
+			'new_post' => array(
+				'post_type' => 'ghost-form-entry',
+				'post_status'	=> 'publish',
+			),
+			'submit_value' => __("Submit", 'ghost-acf-form'),
+			'return' => get_permalink() . '?entry=confirmed#ghost-acf-form',
+			'honeypot' => true,
+			'html_submit_button'	=> '<input type="submit" class="btn btn-primary" value="%s" />',
+			'recaptcha' => true,
 		));
 		$output .= ob_get_clean();
 	}
-    else {
-        $output .= '<div class="card text-center">';
-        	$output .= '<div class="card-body">';
-				$output .= '<strong>Thank you.</strong><br />';
-				$output .= '<br />';
-				$output .= '<strong>Your submission has been recorded.</strong>';
-			$output .= '</div>';
-        $output .= '</div>';
+	else {
+		$output .= '<div class="card text-center">';
+		$output .= '<div class="card-body">';
+		$output .= '<strong>Thank you.</strong><br />';
+		$output .= '<br />';
+		$output .= '<strong>Your submission has been recorded.</strong>';
+		$output .= '</div>';
+		$output .= '</div>';
 	}
 	$output .= '</div>';
 
@@ -271,7 +271,6 @@ function GhostACFForm_render_form($form) {
 
 //Find the relevent form for the form entry
 function GhostACFForm_select_form($groups) {
-
 	if(!is_admin()) {
 		return $groups;
 	}
@@ -298,8 +297,8 @@ function GhostACFForm_select_form($groups) {
 add_filter('acf/get_field_groups', 'GhostACFForm_select_form');
 
 function GhostACFForm_session_start(){
-    if( ! session_id() ) {
-        session_start();
-    }
+	if( ! session_id() ) {
+		session_start();
+	}
 }
 add_action('init', 'GhostACFForm_session_start');
